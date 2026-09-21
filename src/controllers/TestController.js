3ac18780258c137ecd62dc50f16a3dae75fdc30e
Lâm GS <nghiemkhaclam185.mta@gmail.com>
2026-09-21 23:02:45 +0700

    update ver

--- src/controllers/TestController.js
+++ src/controllers/TestController.js
@@ -12,7 +12,7 @@ const TestController = {
             const appName = query.appName || ""
             switch (appName) {
                 case "TIKLUY":
-                    versionApp = "1.5.0"
+                    versionApp = "1.5.3"
                     break;
                 case "VNFITE":
                     versionApp = "1.5.4"
